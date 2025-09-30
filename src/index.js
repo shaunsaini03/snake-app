3b05975056605c3ad00a9980993588c4b0ab5bbd
Shaun Saini <shaunsaini03@yahoo.com>
2025-09-29 17:43:16 -0700

    changed to HashRouter to support gh-pages basename of /snake-app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import reportWebVitals from "./reportWebVitals";
 
